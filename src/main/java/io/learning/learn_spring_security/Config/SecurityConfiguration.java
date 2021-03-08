3d351a2ed6cb2c83860fb34e6ad0bfcdb11da3cb
bnorthern42 <banorthern42@tntech.edu>
2021-03-08 06:31:16 -0600

    end of video 3

---
+++ src/main/java/io/learning/learn_spring_security/Config/SecurityConfiguration.java
@@ -0,0 +1,38 @@
+package io.learning.learn_spring_security.Config;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@EnableWebSecurity
+public class SecurityConfiguration  extends WebSecurityConfigurerAdapter{
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception{
+      //  super.configure(auth);
+        //set config on the auth object
+        auth.inMemoryAuthentication()
+                .withUser("admin")
+                .password("admin") /*8c6976e5b5410415bde908bd4dee15dfb167a9c873fc4bb8a81f6f2ab448a918*/
+                .roles("ADMIN")
+                .and()
+                .withUser("jdoe")
+                .password("jdoe")
+                .roles("USER");
+
+
+
+
+
+    }
+
+    @Bean
+    public PasswordEncoder getPasswordEncoder(){
+        return NoOpPasswordEncoder.getInstance(); //don't do this in prod. just for tut
+    }
+
+}
--- src/main/java/io/learning/learn_spring_security/LearnSpringSecurityApplication.java
+++ src/main/java/io/learning/learn_spring_security/LearnSpringSecurityApplication.java
@@ -3,6 +3,7 @@ package io.learning.learn_spring_security;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class LearnSpringSecurityApplication {
 
