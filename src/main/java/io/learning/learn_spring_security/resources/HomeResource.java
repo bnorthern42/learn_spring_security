b57017d919e0478134bcc6fe798c7e59e6e594e7
bnorthern42 <banorthern42@tntech.edu>
2021-03-08 06:10:43 -0600

    end of video 3

---
+++ src/main/java/io/learning/learn_spring_security/resources/HomeResource.java
@@ -0,0 +1,13 @@
+package io.learning.learn_spring_security.resources;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeResource {
+
+    @GetMapping("/")
+    public String home(){
+        return ("<h1>WELCOME HOME!</h1>");
+    }
+}
